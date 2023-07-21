9e3eca1ee64d1372e81237b5afc65798528e0c3b
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-07-21 08:06:26 +0300

    Создали массив и вывели его в терминал

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int Prompt(string msg)
+{
+  Console.Write(msg);
+  string value = Console.ReadLine();
+  int result = int.Parse(value);
+  return result;
+}
+
+string[] AS; // массив строк
+int count; // длина массива
+string s; // дополнительная переменная-строка
+
+// 2. Ввести количество строк
+
+count = Prompt("Введите длину массива: ");
+
+// 3. Выделить память для массива из count строк
+AS = new string[count];
+
+// 4. Ввести данные массива с клавиатуры
+Console.WriteLine("Enter array:");
+
+for (int i=0; i<AS.Length; i++)
+{
+  Console.Write("AS[{0}] = ", i);
+  AS[i] = Console.ReadLine();
+}
+
+// 5. Сортировка методом вставки
+for (int i = 0; i < AS.Length - 1; i++)
+  for (int j = i; j >= 0; j--)
+    if (String.Compare(AS[j], AS[j + 1]) > 0) // функция Compare()
+    {
+      // поменять значения местами
+      s = AS[j];
+      AS[j] = AS[j + 1];
+      AS[j + 1] = s;
+    }
+
+// 6. Вывести массив AS
+Console.WriteLine("Sorted array:");
+Console.WriteLine($"[{string.Join(", ",AS)}]");
+
+Console.ReadKey();
+
